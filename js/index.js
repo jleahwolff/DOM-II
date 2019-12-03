3c57b5bc237735d1671a9374a44f089a351a7ab6
Jessica Wolff <jleahwolff@gmail.com>
2019-12-03 15:18:26 -0600

    Done with three of ten Events

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 // Your code goes here
+//photoChange---------------------------------------
 const photoChange = document.querySelector('img');
 
-// photoChange.addEventListener('mouseenter', () => {
-//     photoChange.style.transform = 'scale(0.95)'
-//     photoChange.style.transition = 'transform 0.3s'
-// });
-
-// photoChange.addEventListener('mouseleave', () => {
-//     photoChange.style.transform = 'scale(1)'
-// });
-
 photoChange.addEventListener('click', () => {
-    photoChange.src="../img/destination.jpg"
+    photoChange.src="../img/duluth-lighthouse.jpeg"
 });
 
+photoChange.addEventListener('mouseenter', () => {
+    photoChange.style.transform = 'scale(1.1)'
+    photoChange.style.transition = 'transform 0.3s'
+});
+
+photoChange.addEventListener('mouseleave', () => {
+    photoChange.style.transform = 'scale(1)'
+});
 // photoChange.addEventListener('mouseenter', () => {
 //     photoChange.src="..img/fun.jpg"
 // });
+
+//-------------------------------------------------
+
+
