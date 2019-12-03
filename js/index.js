e3c28415283e0dd6873286eae2fd3cb2331a1b0e
Jessica Wolff <jleahwolff@gmail.com>
2019-12-03 15:00:48 -0600

    Save number four

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 // Your code goes here
 const photoChange = document.querySelector('img');
-photoChange.addEventListener('mouseover', );
+
+// photoChange.addEventListener('mouseenter', () => {
+//     photoChange.style.transform = 'scale(0.95)'
+//     photoChange.style.transition = 'transform 0.3s'
+// });
+
+// photoChange.addEventListener('mouseleave', () => {
+//     photoChange.style.transform = 'scale(1)'
+// });
+
+photoChange.addEventListener('click', () => {
+    photoChange.src="../img/destination.jpg"
+});
+
+// photoChange.addEventListener('mouseenter', () => {
+//     photoChange.src="..img/fun.jpg"
+// });
